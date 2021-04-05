648ee309406165a063a898bed9aee9919a0ea71d
Michael Smith <a1721404@adelaide.edu.au>
2021-04-06 03:57:07 +0930

    changed main

--- main.cpp
+++ main.cpp
@@ -4,32 +4,14 @@
 #include "EfficientTruckloads.h"
 
 int main(){
-    int mode;
     Reverse rev;
     TruckLoads truck;
     EfficientTruckLoads efftruck;
-    std::cout << "enter test, 1= reverse digit 2= reverse string 3= truckloads 4= EffTruckloads:";
-    std::cin >> mode;
-    switch (mode)
-    {
-    case 1:
-        std::cout << "input 12345 expect 54321" << std::endl;
-        std::cout << "output :" << rev.reverseDigit(12345) << std::endl;
-        break;
-    case 2:
-        std::cout << "input hello expect olleh" << std::endl;
-        std::cout << "output :" << rev.reverseString("hello") << std::endl;
-        break;
-    case 3:
-        std::cout << "input 14,3 expect 6" << std::endl;
-        std::cout << "output :" << truck.numTrucks(14,3) << std::endl;
-        break;
-    case 4:
-        std::cout << "input 1024,5 expect 256" << std::endl;
-        std::cout << "output :" << efftruck.numTrucks(1024,5) << std::endl;
-        break;
-    default:
-        break;
-    }
+    int i;
+    std::string s;
+    int numCrates;
+    int loadSize;
+    std::cin >> i >> s >> numCrates >> loadSize;
+    std::cout << rev.reverseDigit(i) << " " << rev.reverseString(s) << " " << truck.numTrucks(numCrates,loadSize) << " " << efftruck.numTrucks (numCrates,loadSize);
     return 0;
 }
