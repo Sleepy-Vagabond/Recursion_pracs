aee0efb8917f7dc446004af54420c772657fabda
Michael Smith <a1721404@adelaide.edu.au>
2021-06-26 08:58:09 +0930

    mm

---
+++ LinkedList.h
@@ -0,0 +1,25 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "Node.h"
+
+class LinkedList
+{
+private:
+    Node* head = nullptr;
+public:
+    LinkedList(/* args */);
+    ~LinkedList();
+};
+
+LinkedList::LinkedList(/* args */)
+{
+}
+
+LinkedList::~LinkedList()
+{
+}
+
+
+
+#endif
